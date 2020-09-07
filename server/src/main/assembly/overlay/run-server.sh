8504eda02eb2aa24ea7f6d9a6bdd88f470a986f8
gbarbieri <gbarbieri@itba.edu.ar>
2020-09-07 15:52:44 -0300

    base pod archetype

---
+++ server/src/main/assembly/overlay/run-server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+java  -cp 'lib/jars/*' "ar.edu.itba.g5.server.Server" $*
+
---
+++ server/src/main/java/ar/edu/itba/g5/server/Server.java
@@ -0,0 +1,12 @@
+package ar.edu.itba.g5.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private static Logger logger = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        logger.info("elecciones Server Starting ...");
+    }
+}
